d8a2fc40a00f1662566a5972f8c46e47da9de9d2
Marvin <admin@lishixin.net>
2023-12-24 00:22:49 +0800

    add build img command

--- run_all.sh
+++ run_all.sh
@@ -30,3 +30,8 @@ echo "######### Build RPI0 FW"
 MIX_TARGET=rpi MIX_ENV=prod mix deps.get
 MIX_TARGET=rpi MIX_ENV=prod mix compile --force
 MIX_TARGET=rpi MIX_ENV=prod mix firmware
+
+echo "######## Build IMG"
+fwup -S -s /opt/farmlab_os/priv/prod.pub -i /opt/farmlab_os/_build/rpi4/rpi4_prod/nerves/images/farmbot.fw -o /opt/farmlab_os/_build/rpi4/rpi4_prod/nerves/images/farmlab-rpi4-$(cat VERSION).fw
+fwup -a -t complete -i /opt/farmlab_os/_build/rpi4/rpi4_prod/nerves/images/farmlab-rpi4-$(cat VERSION).fw -d /opt/farmlab_os/farmlab-rpi4-$(cat VERSION).img
+sha256sum /opt/farmlab_os/farmlab-rpi4-$(cat VERSION).img > /opt/farmlab_os/farmlab-rpi4-$(cat VERSION).sha256
